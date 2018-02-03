c472e8fe6ab702def62a82770870ae07346fb4fe
C. Brenhin Keller <cbkeller@princeton.edu>
2018-02-02 18:58:13 -0800

    Add some explanatory comments to Fe3/FeT calculations

--- igneous/FeSpeciation.m
+++ igneous/FeSpeciation.m
@@ -1,10 +1,13 @@
 
 %% Load igneous data and check to exclude samples with blanket Fe3/Fe2 ratio 
-if ~exist('mcign','var'); load mcign; end
+if ~exist('mcign','var'); load mcign; end % Note: this file must be generated with mctest.m: it is too large to store on GitHub
 if ~exist('ign','var'); load ign; end
+if ~exist('igntext','var'); load igntext; end
 
+% Exclude samples with no Fe2/3 data, samples below sea level
 t = ~isnan(ign.Fe2O3) & ~isnan(ign.FeO) & ign.Elevation>-100;
 
+% Fill temporary data structure
 data = struct;
 data.Kv = ign.Kv(t);
 data.FeO = ign.FeO(t);
@@ -15,11 +18,11 @@ data.Longitude = ign.Longitude(t);
 data.Reference = igntext.Reference(t);
 data.elements=fieldnames(data);
 
-
+% List of references
 refs = unique(data.Reference);
 
+% Create and fill array of relative variance for each reference
 relrefvar = NaN(length(refs),1);
-
 for i=1:length(refs)
     t = strcmp(refs{i},data.Reference);
     if sum(t)>1
@@ -27,24 +30,25 @@ for i=1:length(refs)
     end
 end
 
+% Bad references are those with a relative variance less than 0.01
 badrefs = refs(relrefvar<0.01);
 
+% List of sample IDs (kv) corresponding to these excluded references
 badkvs = [];
 for i=1:length(badrefs)
     badkvs = unique([badkvs; ign.Kv(strcmp(badrefs{i},data.Reference))]);
 end
 
+% How many data points are we excluding?
 NExcludedPoints = length(badkvs)
-       
+  
+% Find the samples with excluded IDs in the resampled (mc) dataset
 badkvtest = false(size(mcign.Fe2O3));
 for i=1:length(badkvs)
     badkvtest = badkvtest | (mcign.Kv == badkvs(i));
 end
 
-% Find which samples are volcanic vs. plutonic
-% if ~(isfield(mcign,'p') && isfield(mcign,'v'))
-    if ~exist('igntext','var'); load igntext; end
-    
+% Find which samples are volcanic vs. plutonic 
     Kv = str2double(regexprep(igntext.KV,'KV',''));
     v = strcmpi('volcanic',igntext.Type);
     p = strcmpi('plutonic',igntext.Type);
@@ -53,7 +57,13 @@ end
     
     mcign.p = p(i);
     mcign.v = v(i);
-% end
+
+
+% Calculate weight Fe (without oxide) for Fe3/FeT ratio
+mcign.Fe3 = mcign.Fe2O3 .* 55.845 ./ (55.845 + 1.5*15.9994;
+mcign.Fe2 = mcign.FeO .* 55.845 ./ (55.845 + 15.9994);
+mcign.FeT = mcign.Fe3 + mcign.Fe3;
+
 
 %% Check subaerial fraction
 
@@ -74,27 +84,24 @@ agemax=4000;
 nbins=40;
 Ratio = 0;
 
-
-mcign.Fe3 = mcign.Fe2O3 ./ (55.845 + 1.5*15.9994) *10;
-mcign.Fe2 = mcign.FeO ./ (55.845 + 15.9994) *10;
-mcign.FeT = mcign.Fe3 + mcign.Fe3;
-
-
-data = struct;
-
-% rsi=[43,51,62,74,80];
+% Silica range (or set of ranges) to examine
 rsi=[40,80];
+
+% Temporary struct to hold data for export
+data = struct;
 for i=1:length(rsi)-1
-    test=mcign.SiO2>rsi(i)&mcign.SiO2<rsi(i+1)&mcign.Elevation>-100&~isnan(mcign.(Num))&~isnan(mcign.(Den))&mcign.p&~badkvtest&mcign.FeOT>1&mcign.FeOT<18;
-%     test=mcign.SiO2>rsi(i)&mcign.SiO2<rsi(i+1)&mcign.Elevation>-100&~isnan(mcign.(Num))&~isnan(mcign.(Den))&mcign.p&mcign.FeOT>1&mcign.FeOT<18;
+    % Identify samples of interest
+    test=mcign.SiO2>rsi(i)&mcign.SiO2<rsi(i+1)&mcign.Elevation>-100&~isnan(mcign.(Num))&~isnan(mcign.(Den))&mcign.p&~badkvtest&mcign.FeOT>1&mcign.FeOT<18; % Exclude low-variance datasets that are likely to have applied an arbitrary blanket Fe2/3 ratio
+    % test=mcign.SiO2>rsi(i)&mcign.SiO2<rsi(i+1)&mcign.Elevation>-100&~isnan(mcign.(Num))&~isnan(mcign.(Den))&mcign.p&mcign.FeOT>1&mcign.FeOT<18; % Alternatively, don't exclude low-variance data. Uncomment to use.
     
+    % Calculate and export binned average SiO2 and FeOT
     [c,mt,~]=bin(mcign.Age(test),mcign.SiO2,agemin,agemax,length(mcign.SiO2)./length(ign.SiO2),nbins);
     data.Age = c;
     data.PlutonicSiO2Ave = mt;
     [~,mt,~]=bin(mcign.Age(test),mcign.FeOT,agemin,agemax,length(mcign.SiO2)./length(ign.SiO2),nbins);
     data.PlutonicFeOTAve = mt;
 
-    
+    % Calculate and export binned average Fe3/FeT
     [c,m,e]=bin(mcign.Age(test),mcign.(Num)(test)./(mcign.(Num)(test)+mcign.(Den)(test)),agemin,agemax,length(mcign.SiO2)./length(ign.SiO2),nbins); if Ratio; [m e]=fracttoratio(m,e); end
     figure; errorbar(c,m,2*e,'.b')
     title([num2str(rsi(i)) '-' num2str(rsi(i+1)) '% SiO2 Plutonic']); xlabel('Age (Ma)');
@@ -105,32 +112,36 @@ for i=1:length(rsi)-1
     data.PlutonicFe3_FeT = m;
     data.PlutonicFe3_FeT_Sigma = e;
     
-%     set(gca,'xdir','reverse')
-
-%     binwidth=(agemax-agemin)/nbins;
-%     binedges=linspace(agemin,agemax,nbins+1);
-%     bincenters=agemin+binwidth/2:binwidth:agemax-binwidth/2;
-%     
-%     x = mcign.Age(test);
-%     for j=1:nbins
-%         t = x>binedges(j)&x<binedges(j+1);
-%         N(j)=sum(t);
-% %         Kvs{j} = mcign.Kv(t);
-%         Nunique(j) = length(unique(mcign.Kv(t)));
-%     end
+    
+    % % Calculate number of unique samples in each bin  
+    % binwidth=(agemax-agemin)/nbins;
+    % binedges=linspace(agemin,agemax,nbins+1);
+    % bincenters=agemin+binwidth/2:binwidth:agemax-binwidth/2;
+    % 
+    % x = mcign.Age(test);
+    % for j=1:nbins
+    %     t = x>binedges(j)&x<binedges(j+1);
+    %     N(j)=sum(t);
+    %     % Kvs{j} = mcign.Kv(t);
+    %     Nunique(j) = length(unique(mcign.Kv(t)));
+    % end
     
 end
 ylim([0.15 0.55])
 
-
+% % Plot number of unique samples in each bin
 % figure; plot(c,N)
 % figure; plot(c,Nunique)
 
 
+% Volcanic samples
 rsi=[40,80];
 for i=1:length(rsi)-1
-    test=mcign.SiO2>rsi(i)&mcign.SiO2<rsi(i+1)&mcign.Elevation>-100&~isnan(mcign.(Num))&~isnan(mcign.(Den))&mcign.v&~badkvtest&mcign.FeOT>1&mcign.FeOT<18;
-%     test=mcign.SiO2>rsi(i)&mcign.SiO2<rsi(i+1)&mcign.Elevation>-100&~isnan(mcign.(Num))&~isnan(mcign.(Den))&mcign.v&mcign.FeOT>1&mcign.FeOT<18;
+    % Identify samples of interest
+    test=mcign.SiO2>rsi(i)&mcign.SiO2<rsi(i+1)&mcign.Elevation>-100&~isnan(mcign.(Num))&~isnan(mcign.(Den))&mcign.v&~badkvtest&mcign.FeOT>1&mcign.FeOT<18; % Exclude low-variance datasets that are likely to have applied an arbitrary blanket Fe2/3 ratio
+    % test=mcign.SiO2>rsi(i)&mcign.SiO2<rsi(i+1)&mcign.Elevation>-100&~isnan(mcign.(Num))&~isnan(mcign.(Den))&mcign.v&mcign.FeOT>1&mcign.FeOT<18; % Alternatively, don't exclude low-variance data. Uncomment to use.
+
+
 
     data.VolcanicSiO2Ave = mt;
     [~,mt,~]=bin(mcign.Age(test),mcign.FeOT,agemin,agemax,length(mcign.SiO2)./length(ign.SiO2),nbins);
@@ -142,7 +153,6 @@ for i=1:length(rsi)-1
     if Ratio; ylabel([Num ' / ' Den]); else ylabel([Num ' / ' Num ' + ' Den]); end
     formatfigure
     xlim([agemin agemax])
-%     set(gca,'xdir','reverse')
 
     data.VolcanicFe3_FeT = m2;
     data.VolcanicFe3_FeT_Sigma = e2;
@@ -154,12 +164,9 @@ ylim([0.15 0.55])
 
 data.elements = fieldnames(data);
 exportdataset(data,'1.FilteredFe3_FeT1-18.csv',',');
-% exportdataset(data,'2.UnfilteredFe3_FeT.csv',',');
+% exportdataset(data,'2.UnfilteredFe3_FeT.csv',','); % Uncomment this if using second test string
 
-%% Bin Fe3/FeT, weighted by FeOT
-mcign.Fe3 = mcign.Fe2O3 ./ (55.845 + 1.5*15.9994) * 10;
-mcign.Fe2 = mcign.FeO ./ (55.845 + 15.9994) * 10;
-mcign.FeT = mcign.Fe3 + mcign.Fe3;
+%% Bin Fe3/FeT, weighted by FeOT (experimental)
 
 Num='Fe3';
 Den='Fe2';
@@ -190,11 +197,10 @@ for i=1:length(rsi)-1
     if Ratio; ylabel([Num ' / ' Den]); else ylabel([Num ' / ' Num ' + ' Den]); end
     formatfigure
     xlim([agemin agemax])
-%     set(gca,'xdir','reverse')
 end
 
 
-%% Fe oxidation weighted by sensitivity
+%% Fe oxidation weighted by sensitivity (experimental)
 
 mcign.FeOxidationMolKg = mcign.FeT.*(mcign.Fe3./(mcign.Fe3+mcign.Fe2)-0.22);
 
@@ -203,11 +209,12 @@ agemin=0;
 agemax=4000;
 nbins = 40;
 
-% rsi=[43,51,62,74,80];
+% Silica range(s) to explore
 rsi=[40,80];
+
 i = 1;
 test=mcign.SiO2>rsi(i)&mcign.SiO2<rsi(i+1)&mcign.Elevation>-100&~isnan(mcign.(Elem)) & mcign.p &mcign.FeOT>1&mcign.FeOT<18;
-%test=test&~badkvtest;
+%test=test&~badkvtest; 
 [c,m,e]=binweighted(mcign.Age(test),mcign.(Elem)(test),1./(mcign.FeOT(test)),agemin,agemax,length(mcign.SiO2)./length(ign.SiO2),nbins);
 figure; errorbar(c,m,2*e,'.b')
 title([num2str(rsi(i)) '-' num2str(rsi(i+1)) '% SiO2 Plutonic']); xlabel('Age (Ma)'); ylabel('Fe oxidation (mol/kg)')
@@ -234,9 +241,8 @@ data.VolcanicFeOxidation_Sigma = e;
 data.elements = fieldnames(data);
 exportdataset(data,'5.FeOxidationMol_kg.csv',',');
 
-%     set(gca,'xdir','reverse');
 
-%% Differentiation as a function of time bin
+%% Differentiation as a function of time bin (experimental)
 rt=[0,541,1000,2500,4000]; % Time ranges
 
 mcign.Fe3 = mcign.Fe2O3 ./ (55.845 + 1.5*15.9994) * 10;
@@ -289,7 +295,7 @@ data.elements = fieldnames(data);
 exportdataset(data,'4.Fe3_FeTvsFeOT.csv',',');
 
 ylim([0 0.8])
-ylabel('Fe2O3/(FeO+Fe2O3)'); xlabel('FeO*');
+ylabel('Fe3+/FeT'); xlabel('FeO*');
 legend(l); title('Plutonic')
 formatfigure;
 
