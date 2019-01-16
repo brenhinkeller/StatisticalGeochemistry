431a9dff9613396d101911bfb6c27ad99e075e13
C. Brenhin Keller <cbkeller@berkeley.edu>
2019-01-15 18:00:38 -0800

    Formatfigure now makes pdfs crop properly, and forces vector output

--- functions/plotting/formatfigure.m
+++ functions/plotting/formatfigure.m
@@ -1,28 +1,36 @@
-function formatfigure(varargin) %Format figure font size to 
-% appropriate standards for inclusion in a problem set report
+function formatfigure(varargin) 
+    % Format figure font size to appropriate standards for 
+    % inclusion in a paper or problem set report
 
-if nargin==0;   %If no graphics handle is specified
-    h=get(gca,'title');
-    set(h,'FontSize',22)    %Set font size of figure title
-    
-    h=get(gca,'xlabel');
-    set(h,'FontSize',22)    %Set font size of x label
-    
-    h=get(gca,'ylabel');
-    set(h,'FontSize',22)    %Set font size of y label
-    
-    set(gca,'FontSize',18)  %Set font size of all other text
-    
-else            %If a graphics handle is specified
-    h=get(varargin{:},'title');
-    set(h,'FontSize',22)            %Set font size of figure title
-    
-    h=get(varargin{:},'xlabel');
-    set(h,'FontSize',22)            %Set font size of x label
-    
-    h=get(varargin{:},'ylabel');
-    set(h,'FontSize',22)            %Set font size of y label
-    
-    set(varargin{:},'FontSize',18)  % Set font size of all other text
-    
+    if nargin==0  
+       % If no graphics handle is specified
+       ax = gca();
+    else
+       % If a graphics handle is specified
+       ax = varargin{1};
+    end
+
+    % Set font size of figure title
+    h=get(ax,'title');
+    set(h,'FontSize',16)            
+
+    % Set font size of x label
+    h=get(ax,'xlabel');
+    set(h,'FontSize',16)           
+
+    % Set font size of y label
+    h=get(ax,'ylabel');
+    set(h,'FontSize',16)            
+
+    % Set font size of all other text
+    set(ax,'FontSize',14)  
+
+    % Crop figure to scale
+    fig = gcf();
+    warning('off', 'MATLAB:print:FigureTooLargeForPage')
+    fig.PaperSize = [fig.PaperPosition(3) fig.PaperPosition(4)];
+
+    % Ensure vector output
+    set(fig,'renderer','painters')
+
 end
