617614f76d449a2b3135c3ccc7affbc02a3b65d8
C. Brenhin Keller <cbkeller@berkeley.edu>
2019-01-15 18:06:53 -0800

    More averages to export

--- igneous/ExportBinnedMCAverages.m
+++ igneous/ExportBinnedMCAverages.m
@@ -12,11 +12,13 @@ mcign=findOIBs(mcign);
 %% Elements to export
 elements={'CaO';'K2O';'SiO2';'MgO';'Rb';'Sr'};
 
+% Options include:'SiO2';'TiO2';'Al2O3';'Fe2O3';'Fe2O3T';'FeO';'FeOT';'MgO';'CaO';'Na2O';'K2O';'P2O5';'MnO';'H2O_Total';'La';'Ce';'Pr';'Nd';'Sm';'Eu';'Gd';'Tb';'Dy';'Ho';'Er';'Tm';'Yb';'Lu';'Li';'Be';'B';'C';'CO2';'F';'Cl';'Sc';'Ti';'V';'Cr';'Co';'Ni';'Cu';'Zn';'Ga';'Zr';'Os';'Rb';'Bi';'Hg';'Ba';'Y';'Pb';'Te';'Nb';'Sr87_Sr86';'Tl';'Pt';'Sn';'Cd';'As';'Pd';'Sr';'Se';'S';'Au';'Ta';'Mo';'U';'Cs';'Sb';'Ag';'W';'Th';'Re';'Hf';'Ir';
+
 %% Timeseries properties
 agemin=0;
 agemax=4000;
 timestep=100;
-nbins=(agemax-agemin)/timestep+1;
+nbins=(agemax-agemin)/timestep;
 
 %% Binned average for silica ranges
 
@@ -24,7 +26,7 @@ SiRange=[43,51,62,74,80];
 
 for i=1:(length(SiRange)-1)
     output = zeros(nbins,2*length(elements)+1);
-    for j = 1:length(elements);
+    for j = 1:length(elements)
         elem=elements{j};
         test=~isnan(mcign.(elem)) & mcign.SiO2>SiRange(i) & mcign.SiO2<SiRange(i+1) & mcign.Elevation>-100 & ~mcign.oibs;
         [c,m,e]=bin(mcign.Age(test),mcign.(elem)(test),agemin,agemax,length(mcign.SiO2)./length(ign.SiO2),nbins);
